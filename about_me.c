31d80f9de511181280d7a30d072b21b9a285def3
Riva Islam <rivakhan569@gmail.com>
2026-08-25 21:27:36 +0500

    about_me.c

---
+++ about_me.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("I am Riva.");
+    printf("I am 19 years old.");
+    printf("I live in Chandpur.");
+    printf("I am a first-year CSE student at Daffodil International College, Baburhat.");
+    printf("My father's name is Soleman Khan and he is a businessman.");
+    printf("My mother's name is Sonoara Begum and she is a housewife.");
+    printf("I completed my secondary education at Farakkabad High Scholl.");
+    printf("I also completed my higher secondary education at Farakkabad Degree College.");
+    printf("I want to improve my coding and English-speaking skills.");
+    printf("I focus seriously on my studies and I want to become a Software Developer in the future.");
+
+
+    return 0;
+
+
+}
