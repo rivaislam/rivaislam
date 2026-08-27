fa98aee95614061b5fca0d2921962f5b66d6a970
Riva Islam <rivakhan569@gmail.com>
2026-08-27 11:18:48 +0500

    character.c

---
+++ character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+    char vowel='a';
+    char vowel1= 'e';
+    char vowel2='i';
+    char vowel3='o';
+    char vowel4='u';
+
+    printf("vowel:%c\n%c\n%c\n%c\n%c\n", vowel,vowel1,vowel2,vowel3,vowel4);
+
+    return 0;
+
+}
