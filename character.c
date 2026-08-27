9c8a35604f1e780a7e498331689941bb1a30c83b
Riva Islam <rivakhan569@gmail.com>
2026-08-27 11:12:43 +0500

    character.c

---
+++ character.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+    int a=20;
+    int b=10;
+    int sum;
+    int sub;
+    int mult;
+    int div;
+
+    sum= a+b;
+    printf("sum:%d\n",sum);
+
+    sub= a-b;
+    printf("sub:%d\n",sub);
+
+    mult= a*b;
+    printf("mult:%d\n",mult);
+
+    div= a/b;
+    printf("div:%d\n",div);
+
+
+    return 0;
+}
